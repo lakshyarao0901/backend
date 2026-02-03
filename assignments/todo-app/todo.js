8d51a70402f8299920ec5ab18792ffc246ad9e44
Lakshya <lakshya.yad@gmail.com>
2026-02-03 14:44:11 +0530

    sir uploaded this code

---
+++ assignments/todo-app/todo.js
@@ -0,0 +1,24 @@
+const fs =require("fs");
+const path =require("path");
+
+const TODO_FILE = path.join(__dirname,"todos.json");
+
+// console.log(TODO_FILE);
+
+// fs.unlink("file.txt",(err) => {
+// if (err) console.log(err.message);
+// // console.log("File deleted");
+// });
+
+// fs.stat("todos.json", (err, stat) => {
+//     if (err) console.log(err.message);
+//     else console.log(stat.isFile());
+    
+// })
+
+fs.mkdir("a/b/c", (err) => {
+    console.log(err);
+    
+});
+
+console.log("hello")
